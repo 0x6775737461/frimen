0cf823f7d29094f940166c82f1da425220fbadbd
0x6775737461 <fabiogustavo@tuta.io>
2022-05-18 07:44:46 -0400

    putting parentheses in the right place

--- serial/main.c
+++ serial/main.c
@@ -81,7 +81,7 @@ int main(void) {
   initial_time = clock() - initial_time;
 
   printf("Time Elapsed: [%.2f] min\n",
-    (((double)initial_time) / CLOCKS_PER_SEC)) / 60;
+    (((double)initial_time) / CLOCKS_PER_SEC) / 60);
 
   free(matrix);
   free(pair_addr);
